f727ee13fcd45398ede935d28eda1736e2ae4065
sawaivana <136937542+sawaivana@users.noreply.github.com>
2024-05-18 18:13:17 -0500

    Add files via upload

---
+++ testingjava.js
@@ -0,0 +1,7 @@
+<script>
+var aRCHiVEText = document.getElementById("aRCHiVEText");
+if (aRCHiVEText) {
+  aRCHiVEText.addEventListener("click", function (e) {
+    // Please sync "Archive" to the project
+  });
+</script>
